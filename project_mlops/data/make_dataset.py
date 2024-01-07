f4bf946fd826b5dcce1090f477df891eca01c729
Thomas Swagerman <thomasswagerman@hotmail.com>
2024-01-07 11:58:31 +0100

    dvc setup

--- project_mlops/data/make_dataset.py
+++ project_mlops/data/make_dataset.py
@@ -1,14 +1,26 @@
-import numpy as np
-import pandas as pd 
+import subprocess
 import os
+import pandas as pd 
+
+def get_dvc_remote_path(remote_name):
+    result = subprocess.run(['dvc', 'remote', 'default', remote_name], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        raise RuntimeError(f"Failed to get DVC remote path: {result.stderr.strip()}")
 
 def get_data():
-    local_dvc_repo = '<path-to-local-dvc-repo>'
-    os.system(f'dvc pull -r myremote')
+    # Run DVC pull to fetch data from the remote
+    os.system('dvc pull -r public-remote')
 
-    pass
+    # Retrieve the local DVC cache path from the DVC configuration
+    dvc_remote_path = get_dvc_remote_path('public-remote')
+
+    # Load the CSV file into a Pandas DataFrame
+    csv_file_path = os.path.join(dvc_remote_path, 'data/raw/news.csv')  # Adjust the path to your CSV file
+    df = pd.read_csv(csv_file_path)
+
+    print(df.head())
 
 if __name__ == '__main__':
     get_data()
-    pass['remote "storage"']
-    url = gdrive://1X2P4EfkFSkOlSrUg8ugygr5blUvGojsE
